02541fcc85057f56138c359865bc603e7a65d8f5
Madan-444 <mmadan3600@gmail.com>
2020-02-17 19:08:25 +0545

    Corausel effect

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import Layout from './core/Layout'
+import React from "react";
+import Layout from "./core/Layout";
 
 function App() {
   return (
-    <Layout>
-     <h2>Hello from App </h2>
-    </Layout>
+    <>
+      <Layout>
+        <div>
+          <h2>Hello from App </h2>
+          <p>haha k vako</p>
+          <div
+            id="carouselExampleControls"
+            class="carousel slide"
+            data-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="..." class="d-block w-100" alt="..." />
+              </div>
+              <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="..." />
+              </div>
+              <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="..." />
+              </div>
+            </div>
+            <a
+              class="carousel-control-prev"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a
+              class="carousel-control-next"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 }
 
